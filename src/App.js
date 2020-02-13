3220c9d858f361613cfa8e566abde06d96dd1e92
Ali Najdi <ali.najdi.1989@gmail.com>
2020-02-13 17:50:13 +0330

    removed unnecessary cdns from index.html, added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Admin from "./pages/Admin";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/about' component={About}/>
+                <Route exact path='/admin' component={Admin}/>
+
+                <Route exact path='/signup' component={SignUp}/>
+                <Route exact path='/signin' component={SignIn}/>
+
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
